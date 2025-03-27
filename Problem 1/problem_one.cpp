ec49e46f95639c486be95c31dea3e9bf0bf74613
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-03-27 12:45:01 +0100

    Multiples of 3 or 5

---
+++ Problem 1/problem_one.cpp
@@ -0,0 +1,50 @@
+/* *
+ * Multiples of 3 or 5
+ * -------------------
+ * If we list all the natural numbers below 10
+ * that are multiples of 3 or 5, we get 3, 5,
+ * 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ * ---------------------------------------------------------
+ */
+#include <iostream>
+#include <vector>
+
+#define LOWER_LIMIT 1
+#define UPPER_LIMIT 1000
+
+int main(int argc, char **argv)
+{
+    int sum = 0;
+    std::vector<int> result;
+    for (int i = LOWER_LIMIT; i < UPPER_LIMIT; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            result.push_back(i);
+            sum += i;
+        }
+    }
+
+    int i = 0;
+    for (std::vector<int>::iterator it = result.begin();
+         it != result.end();
+         it++)
+    {
+        std::cout << *it << " ";
+
+        if (i % 16 == 15)
+        {
+            std::cout << "\n";
+        }
+        else if (i % 8 == 7)
+        {
+            std::cout << "   ";
+        }
+
+        i++;
+    }
+    std::cout << "\n"
+              << "Vector size: " << result.size() << "\n"
+              << "Vector sum:" << sum << "\n";
+}
